b567d31a16062a9207c2647023e793298fc11cd9
Dmitry Shevelev <dmitry.shevelev@check24.de>
2025-11-24 00:22:26 +0100

    Make management command universal

--- wool_base/management/commands/refresh_wool_base.py
+++ wool_base/management/commands/refresh_wool_base.py
@@ -5,7 +5,7 @@ from wool_base.scrapers.wollplatz_scraper import WollplatzScraper
 
 
 class Command(BaseCommand):
-    help = 'Scrape products from Wollplatz'
+    help = 'Scrape products from desired shops'
 
     PRODUCTS = [
         {'brand': 'DMC', 'name': 'Natura XL'},
@@ -15,35 +15,45 @@ class Command(BaseCommand):
         {'brand': 'Stylecraft', 'name': 'Special DK'},
     ]
 
+    SHOPS = [
+        {
+            'name': 'Wollplatz',
+            'base_url': 'https://www.wollplatz.de',
+            'scraper_class': WollplatzScraper,
+        },
+    ]
+
     def handle(self, *args, **options):
-        shop, _ = Shop.objects.get_or_create(
-            name='Wollplatz',
-            defaults={'base_url': 'https://www.wollplatz.de'}
-        )
-        
-        scraper = WollplatzScraper(shop)
-        
-        for product_data in self.PRODUCTS:
-            product, _ = Product.objects.get_or_create(
-                brand=product_data['brand'],
-                name=product_data['name']
+        for shop_config in self.SHOPS:
+            shop, _ = Shop.objects.get_or_create(
+                name=shop_config['name'],
+                defaults={'base_url': shop_config['base_url']}
             )
             
-            offer = scraper.scrape_offer(product)
+            scraper = shop_config['scraper_class'](shop)
             
-            if offer:
-                Offer.objects.update_or_create(
-                    product=product,
-                    shop=shop,
-                    defaults={
-                        'price': offer.price,
-                        'availability': offer.availability,
-                        'needle_size': offer.needle_size,
-                        'composition': offer.composition,
-                        'url': offer.url,
-                    }
+            self.stdout.write(f'\nProcessing shop: {shop.name}')
+            
+            for product_data in self.PRODUCTS:
+                product, _ = Product.objects.get_or_create(
+                    brand=product_data['brand'],
+                    name=product_data['name']
                 )
-                self.stdout.write(f'DONE {product.brand} {product.name}')
-            else:
-                self.stdout.write(f'ERROR {product.brand} {product.name} - not found')
-
+                
+                offer = scraper.scrape_offer(product)
+                
+                if offer:
+                    Offer.objects.update_or_create(
+                        product=product,
+                        shop=shop,
+                        defaults={
+                            'price': offer.price,
+                            'availability': offer.availability,
+                            'needle_size': offer.needle_size,
+                            'composition': offer.composition,
+                            'url': offer.url,
+                        }
+                    )
+                    self.stdout.write(f'DONE {product.brand} {product.name}')
+                else:
+                    self.stdout.write(f'ERROR {product.brand} {product.name} - not found')
